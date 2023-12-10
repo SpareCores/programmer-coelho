e211c957383069101418689ec2786dcc6fae9b64
Gergely Daroczi (@daroczig) <daroczig@rapporter.net>
2023-12-10 10:50:57 +0100

    lint line length

--- quote.py
+++ quote.py
@@ -79,7 +79,9 @@ if os.getenv('TWITTER_CLIENT_ID') and \
         access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET']
     )
     if len(quote) > 160:
-        quote = askai('Shorten this text to be less than 158 characters:' + quote).strip('\"')
+        quote = askai(
+            'Shorten this text to be less than 160 characters:'
+            + quote).strip('\"')
     client.create_tweet(text=quote)
 
 # local post
